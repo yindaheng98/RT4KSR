155c6eb757d3cce2191da2925db9d1d94f669085
yindaheng98 <yindaheng98@gmail.com>
2024-05-08 16:48:29 -0700

    train/test

--- scripts/run_train_dual.sh
+++ scripts/run_train_dual.sh
@@ -14,15 +14,33 @@ traindual() {
         --scale=$2 \
         --arch=nerfrt4ksr_rep \
         --benchmark=nerfoutdual_train \
-        --checkpoint-id=$1/nerfrt4ksr_x"$2"_dual
+        --checkpoint-id=$1/nerfrt4ksr_x"$2"
 }
 # traindual coffee_martini-kmeans-6 2 # debug
+testdual() {
+    python code/test.py \
+        --dataroot=data/$1 \
+        --scale=$2 \
+        --arch=nerfrt4ksr_rep \
+        --benchmark=nerfoutdual_train \
+        --checkpoint-id=$1/nerfrt4ksr_x"$2"_rep_model
+}
+# testdual coffee_martini-kmeans-6 2 # debug
 trainsingle() {
     python code/train.py \
         --dataroot=data/$1 \
         --scale=$2 \
         --arch=rt4ksr_rep \
         --benchmark=nerfout_train \
-        --checkpoint-id=$1/rt4ksr_x"$2"_dual
+        --checkpoint-id=$1/rt4ksr_x"$2"
 }
 # trainsingle coffee_martini-kmeans-6 2 # debug
+testsingle() {
+    python code/test.py \
+        --dataroot=data/$1 \
+        --scale=$2 \
+        --arch=rt4ksr_rep \
+        --benchmark=nerfout_train \
+        --checkpoint-id=$1/rt4ksr_x"$2"_rep_model
+}
+# testsingle coffee_martini-kmeans-6 2 # debug
