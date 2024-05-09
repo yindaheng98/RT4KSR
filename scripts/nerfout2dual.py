7136edc3142cbe1a465849e74d7ba0c38e274e4b
yindaheng98 <yindaheng98@gmail.com>
2024-05-08 22:20:04 -0700

    gather low color

--- scripts/nerfout2dual.py
+++ scripts/nerfout2dual.py
@@ -8,6 +8,7 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument("--hrsrcroot", type=str, required=True)  # Full NeRF output
 parser.add_argument("--grsrcroot", type=str, required=True)  # Gray NeRF output
+parser.add_argument("--crsrcroot", type=str, required=True)  # Low-quality color NeRF output
 parser.add_argument("--dataroot", type=str, required=True)
 parser.add_argument("--name", type=str, required=True)
 parser.add_argument("--valratio", type=int, default=4)
@@ -16,29 +17,39 @@ if __name__ == "__main__":
     args = parser.parse_args()
     hr_train_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", "HR")
     gr_train_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", "Gray")
+    cr_train_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", "LR_bicubic_x1")
     os.makedirs(hr_train_dir_path, exist_ok=True)
     os.makedirs(gr_train_dir_path, exist_ok=True)
+    os.makedirs(cr_train_dir_path, exist_ok=True)
     hr_val_dir_path = os.path.join(args.dataroot, "testsets", args.name, "val", "HR")
     gr_val_dir_path = os.path.join(args.dataroot, "testsets", args.name, "val", "Gray")
+    cr_val_dir_path = os.path.join(args.dataroot, "testsets", args.name, "val", "LR_bicubic_x1")
     os.makedirs(hr_val_dir_path, exist_ok=True)
     os.makedirs(gr_val_dir_path, exist_ok=True)
+    os.makedirs(cr_val_dir_path, exist_ok=True)
     i = 0
     for path in glob.glob(os.path.join(args.hrsrcroot, "camera*")):
         for entry in os.scandir(path):
             hr_dir_path = hr_train_dir_path
             gr_dir_path = gr_train_dir_path
+            cr_dir_path = cr_train_dir_path
             if i % args.valratio == 0:
                 hr_dir_path = hr_val_dir_path
                 gr_dir_path = gr_val_dir_path
+                cr_dir_path = cr_val_dir_path
             hrsrc = entry.path
             grsrc = os.path.join(args.grsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
+            crsrc = os.path.join(args.crsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
             ext = os.path.splitext(entry.name)[1]
             hrdst = os.path.join(hr_dir_path, str(i) + ext)
             grdst = os.path.join(gr_dir_path, str(i) + ext)
+            crdst = os.path.join(cr_dir_path, str(i) + ext)
             print(hrsrc, hrdst)
             print(grsrc, grdst)
+            print(crsrc, crdst)
             shutil.copyfile(hrsrc, hrdst)
             shutil.copyfile(grsrc, grdst)
+            shutil.copyfile(crsrc, crdst)
             hr = cv2.imread(hrsrc)
             for scale in [2, 3, 4]:
                 lr_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", f"LR_bicubic_x{scale}")
