99c678f76df62a32dfd3af99325bef1bd3d6b62f
yindaheng98 <yindaheng98@gmail.com>
2024-06-20 16:25:36 -0700

    get data from 3dgs

--- scripts/nerfout2dual.py
+++ scripts/nerfout2dual.py
@@ -1,8 +1,6 @@
 import argparse
-import numpy as np
 import os
 import cv2
-import glob
 import shutil
 
 parser = argparse.ArgumentParser()
@@ -28,37 +26,36 @@ if __name__ == "__main__":
     os.makedirs(gr_val_dir_path, exist_ok=True)
     os.makedirs(cr_val_dir_path, exist_ok=True)
     i = 0
-    for path in glob.glob(os.path.join(args.hrsrcroot, "camera*")):
-        for entry in os.scandir(path):
-            hr_dir_path = hr_train_dir_path
-            gr_dir_path = gr_train_dir_path
-            cr_dir_path = cr_train_dir_path
+    for entry in os.scandir(args.hrsrcroot):
+        hr_dir_path = hr_train_dir_path
+        gr_dir_path = gr_train_dir_path
+        cr_dir_path = cr_train_dir_path
+        if i % args.valratio == 0:
+            hr_dir_path = hr_val_dir_path
+            gr_dir_path = gr_val_dir_path
+            cr_dir_path = cr_val_dir_path
+        hrsrc = entry.path
+        grsrc = os.path.join(args.grsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
+        crsrc = os.path.join(args.crsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
+        ext = os.path.splitext(entry.name)[1]
+        hrdst = os.path.join(hr_dir_path, str(i) + ext)
+        grdst = os.path.join(gr_dir_path, str(i) + ext)
+        crdst = os.path.join(cr_dir_path, str(i) + ext)
+        print(hrsrc, hrdst)
+        print(grsrc, grdst)
+        print(crsrc, crdst)
+        shutil.copyfile(hrsrc, hrdst)
+        shutil.copyfile(grsrc, grdst)
+        shutil.copyfile(crsrc, crdst)
+        hr = cv2.imread(hrsrc)
+        for scale in [2, 3, 4]:
+            lr_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", f"LR_bicubic_x{scale}")
             if i % args.valratio == 0:
-                hr_dir_path = hr_val_dir_path
-                gr_dir_path = gr_val_dir_path
-                cr_dir_path = cr_val_dir_path
-            hrsrc = entry.path
-            grsrc = os.path.join(args.grsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
-            crsrc = os.path.join(args.crsrcroot, os.path.relpath(hrsrc, args.hrsrcroot))
-            ext = os.path.splitext(entry.name)[1]
-            hrdst = os.path.join(hr_dir_path, str(i) + ext)
-            grdst = os.path.join(gr_dir_path, str(i) + ext)
-            crdst = os.path.join(cr_dir_path, str(i) + ext)
-            print(hrsrc, hrdst)
-            print(grsrc, grdst)
-            print(crsrc, crdst)
-            shutil.copyfile(hrsrc, hrdst)
-            shutil.copyfile(grsrc, grdst)
-            shutil.copyfile(crsrc, crdst)
-            hr = cv2.imread(hrsrc)
-            for scale in [2, 3, 4]:
-                lr_dir_path = os.path.join(args.dataroot, "testsets", args.name, "train", f"LR_bicubic_x{scale}")
-                if i % args.valratio == 0:
-                    lr_dir_path = os.path.join(args.dataroot, "testsets", args.name, "val", f"LR_bicubic_x{scale}")
-                os.makedirs(lr_dir_path, exist_ok=True)
-                lrdst = os.path.join(lr_dir_path, str(i) + ext)
-                print(hrsrc, lrdst)
-                lr = cv2.resize(hr, dsize=(hr.shape[1]//scale, hr.shape[0]//scale), interpolation=cv2.INTER_NEAREST)
-                cv2.imwrite(lrdst, lr)
-            print("------")
-            i += 1
+                lr_dir_path = os.path.join(args.dataroot, "testsets", args.name, "val", f"LR_bicubic_x{scale}")
+            os.makedirs(lr_dir_path, exist_ok=True)
+            lrdst = os.path.join(lr_dir_path, str(i) + ext)
+            print(hrsrc, lrdst)
+            lr = cv2.resize(hr, dsize=(hr.shape[1]//scale, hr.shape[0]//scale), interpolation=cv2.INTER_NEAREST)
+            cv2.imwrite(lrdst, lr)
+        print("------")
+        i += 1
