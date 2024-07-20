40ba5c55c67f730aaa3c8357006daf94b25e62b7
yindaheng98 <yindaheng98@gmail.com>
2024-07-20 14:36:04 -0700

    doall_wrap

--- scripts/run_train_dual.sh
+++ scripts/run_train_dual.sh
@@ -32,4 +32,17 @@ testdual() {
         --checkpoint-id=$1/${ARCHDUAL}_x1_rep_model \
         --save-results=srresults/$1-nerfoutdual-x1.json
 }
-testdual coffee_martini-kmeans-qp-none-scale-16-rot-16-f_dc-16-f_rest-16-opacity-4-warped
+# testdual coffee_martini-kmeans-qp-none-scale-16-rot-16-f_dc-16-f_rest-16-opacity-4-warped
+doboth() {
+    traindual $1 64 $2
+    testdual $1
+}
+# doboth coffee_martini-kmeans-qp-none-scale-16-rot-16-f_dc-16-f_rest-16-opacity-4-warped 1176
+doall() {
+    doboth $1-kmeans-$2-warped $3
+    doboth $1-kmeans-$2-warpednoee $3
+}
+doall_wrap() {
+    doall $1 qp-none-scale-$3-rot-$4-f_dc-$5-f_rest-$6-opacity-$7 $2
+}
+doall_wrap coffee_martini 1176 16 16 16 16 4
